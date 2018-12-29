82a2f24a3ebcff8b6826cb8d0c78c7c58300a783
huobb0 <zsombor.kovacs@gmail.com>
2018-12-29 21:54:12 +0100

    Initial Commit

---
+++ automatedScreenshotApp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  automatedScreenshotApp
+//
+//  Created by Zsombor on 2018. 12. 29..
+//  Copyright © 2018. Zsombor. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
